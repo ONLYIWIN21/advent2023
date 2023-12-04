b17688a9ca6bb633c8ed49f9c81f23bb65176cea
ONLYIWIN21 <flintthecuber@gmail.com>
2023-12-04 13:47:09 -0500

    day 04

---
+++ src/day04/mod.rs
@@ -0,0 +1,49 @@
+use crate::get_input;
+
+pub fn solve(part1: bool) -> u32 {
+    let input = get_input!("04");
+
+    let mut sum = 0;
+    let mut scratch_cards: [u64; 213] = [1; 213];
+    let mut card_num = 1;
+    for line in input {
+        if let Ok(line) = line {
+            let mut score = 1;
+            let mut split = line.split("|");
+            let solns: Vec<u32> = split
+                .next()
+                .unwrap()
+                .split_whitespace()
+                .skip(2)
+                .map(|x| x.parse::<u32>().unwrap())
+                .collect();
+
+            let mut num_wins = 0;
+            for guess in split.next().unwrap().split_whitespace() {
+                let guess = guess.parse::<u32>().unwrap();
+                if solns.contains(&guess) {
+                    score *= 2;
+                    num_wins += 1;
+                }
+            }
+
+            for i in (card_num + 1)..=(num_wins + card_num) {
+                scratch_cards[i] += scratch_cards[card_num];
+            }
+
+            sum += score / 2;
+        }
+        card_num += 1;
+    }
+
+    if part1 {
+        return sum;
+    }
+
+    let mut total = 0;
+    for card in scratch_cards {
+        total += card;
+    }
+
+    return (total - 1) as u32;
+}
