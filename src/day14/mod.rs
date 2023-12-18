b8383741c20d3bc29bd9f07acae2af3891eb7735
ONLYIWIN21 <flintthecuber@gmail.com>
2023-12-17 22:17:34 -0500

    day 14 pt 2

--- src/day14/mod.rs
+++ src/day14/mod.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::get_input;
 
 pub fn solve() -> (u32, u32) {
@@ -19,18 +21,75 @@ pub fn solve() -> (u32, u32) {
         }
     }
 
-    for _ in 0..100 { // im lazy
-        for x in 0..map[0].len() {
-            for y in (1..map.len()).rev() {
-                if map[y][x] == 1 && map[y - 1][x] == 2 {
-                    map[y][x] = 2;
-                    map[y - 1][x] = 1;
+    let mut prev = HashMap::new();
+
+    let mut sums = (0, 0);
+    let mut i = 0;
+    while i < 1000000000 {
+        for _ in 0..100 {
+            for x in 0..map[0].len() {
+                for y in (1..map.len()).rev() {
+                    if map[y][x] == 1 && map[y - 1][x] == 2 {
+                        map[y][x] = 2;
+                        map[y - 1][x] = 1;
+                    }
                 }
             }
         }
+
+        if sums.0 == 0 {
+            for y in 0..map.len() {
+                let mut row_count = 0;
+                for x in 0..map[0].len() {
+                    if map[y][x] == 1 {
+                        row_count += 1;
+                    }
+                }
+                sums.0 += row_count * (map.len() - y) as u32;
+            }
+        }
+
+        for _ in 0..100 {
+            for y in 0..map.len() {
+                for x in (1..map[0].len()).rev() {
+                    if map[y][x] == 1 && map[y][x - 1] == 2 {
+                        map[y][x] = 2;
+                        map[y][x - 1] = 1;
+                    }
+                }
+            }
+        }
+
+        for _ in 0..100 {
+            for x in 0..map[0].len() {
+                for y in 0..(map.len() - 1) {
+                    if map[y][x] == 1 && map[y + 1][x] == 2 {
+                        map[y][x] = 2;
+                        map[y + 1][x] = 1;
+                    }
+                }
+            }
+        }
+
+        for _ in 0..100 {
+            for y in 0..map.len() {
+                for x in 0..(map[0].len() - 1) {
+                    if map[y][x] == 1 && map[y][x + 1] == 2 {
+                        map[y][x] = 2;
+                        map[y][x + 1] = 1;
+                    }
+                }
+            }
+        }
+
+        if prev.contains_key(&map) {
+            i = prev[&map] + (1000000000 - prev[&map]) / (i - prev[&map]) * (i - prev[&map]);
+        }
+
+        prev.insert(map.clone(), i);
+        i += 1;
     }
 
-    let mut sum = 0;
     for y in 0..map.len() {
         let mut row_count = 0;
         for x in 0..map[0].len() {
@@ -38,8 +97,8 @@ pub fn solve() -> (u32, u32) {
                 row_count += 1;
             }
         }
-        sum += row_count * (map.len() - y) as u32;
+        sums.1 += row_count * (map.len() - y) as u32;
     }
 
-    return (sum, 0);
+    return sums;
 }
